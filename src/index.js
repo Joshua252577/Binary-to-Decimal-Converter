058dcc364e57788467b5f131ea1c0ac0224e306f
Matheus Marques <99177444matheus@gmail.com>
2023-01-18 22:28:14 -0300

    criando arquivos e estruturando

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const bin = document.querySelector('.binario').value;
+const dec = document.querySelector('.decimal');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('Click', function converter(event, bin) {
+    event.preventDefault();
+    console.log('clicou')
+    
+});
